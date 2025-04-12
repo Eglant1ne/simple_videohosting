be325283e01c56adb7e03ee1f438d3631be9c7f5
kirigaya <valerasaratov064@gmail.com>
2025-04-12 19:19:42 +0300

    добавление password_utils

---
+++ services/auth_service/jwt_tokens/password_utils.py
@@ -0,0 +1,16 @@
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
+
+
+ALGORITHM = "RS256"
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def generate_hashed_password(password):
+    return pwd_context.hash(password)
