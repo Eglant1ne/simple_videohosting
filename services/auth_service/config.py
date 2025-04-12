0e55bf6f5209fd357ae23d4b9083fcb32c8f345f
kirigaya <valerasaratov064@gmail.com>
2025-04-12 19:03:54 +0300

    добавление RSA ключей для JWT токенов

--- services/auth_service/config.py
+++ services/auth_service/config.py
@@ -12,5 +12,11 @@ class DebugMode(BaseSettings):
     debug_mode: bool = Field(alias='DEBUG_MODE')
 
 
+class RSAKeys(BaseSettings):
+    public_key: str = Field(alias="RSA_PUBLIC_KEY")
+    private_key: str = Field(alias="RSA_PRIVATE_KEY")
+
+
 DATABASE_SETTINGS = DatabaseSettings()
 DEBUG_MODE = DebugMode()
+RSA_KEYS = RSAKeys()
