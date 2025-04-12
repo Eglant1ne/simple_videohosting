92efd7c6554621d3d690402f26a71728c869a570
kirigaya <valerasaratov064@gmail.com>
2025-04-12 19:20:35 +0300

    удаление лишнего

--- services/auth_service/jwt_tokens/password_utils.py
+++ services/auth_service/jwt_tokens/password_utils.py
@@ -2,8 +2,6 @@ from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
 
 
-ALGORITHM = "RS256"
-
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
