4cbafdd9f82cb09248bbd71555ad97fde9374bb9
kirigaya <valerasaratov064@gmail.com>
2025-04-14 00:05:44 +0300

    небольшие изменения конфигурации kafka

--- services/channel_actions_service/kafka_producer.py
+++ services/channel_actions_service/kafka_producer.py
@@ -7,5 +7,5 @@ kafka_producer = None
 async def create_producer():
     global kafka_producer
     loop = asyncio.get_running_loop()
-    kafka_producer = AIOKafkaProducer(bootstrap_servers='kafka:9090', enable_idempotence=True, loop=loop)
+    kafka_producer = AIOKafkaProducer(bootstrap_servers='kafka:9092', enable_idempotence=True, loop=loop)
     return kafka_producer
