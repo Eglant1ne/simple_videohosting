ff891c841a6982b169581b8fc6ab84d59af30512
Eglantine <antonscheglow40o@gmail.com>
2025-04-15 22:24:31 +0300

    change file structure, add auth validation

--- services/file_upload_service/internal/handler/upload.go
+++ services/file_upload_service/internal/handler/upload.go
@@ -6,9 +6,9 @@ import (
 	"io"
 	"net/http"
 
+	api "github.com/Eglant1ne/simple_videohosting/services/file_upload_service/api"
 	"github.com/Eglant1ne/simple_videohosting/services/file_upload_service/internal/config"
 	"github.com/Eglant1ne/simple_videohosting/services/file_upload_service/internal/service"
-	response "github.com/Eglant1ne/simple_videohosting/services/file_upload_service/pkg/http"
 	"github.com/minio/minio-go/v7"
 )
 
@@ -16,24 +16,33 @@ const defaultPartSize = 32 << 20
 
 func UploadHandler(minioSvc *service.MinIOService, cfg *config.Config) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		r.Body = http.MaxBytesReader(w, r.Body, 10<<30)
+		access_token, err := getCookieHandler(w, r)
+		if err != nil {
+			api.JSONResponse(w, http.StatusUnauthorized, "Не авторизованный пользователь!")
+			return
+		}
+		if api.IsAuthenticated(access_token) != 200 {
+			return
+		}
+
+		r.Body = http.MaxBytesReader(w, r.Body, 20<<30)
 		reader, err := r.MultipartReader()
 		if err != nil {
-			response.JSONResponse(w, http.StatusBadRequest, fmt.Sprintf("Ошибка чтения файла: %v", err))
+			api.JSONResponse(w, http.StatusBadRequest, fmt.Sprintf("Ошибка чтения файла: %v", err))
 			return
 		}
 
 		part, err := reader.NextPart()
 		if err != nil {
-			response.JSONResponse(w, http.StatusBadRequest, fmt.Sprintf("Ошибка чтения части: %v", err))
+			api.JSONResponse(w, http.StatusBadRequest, fmt.Sprintf("Ошибка чтения части: %v", err))
 		}
 		for {
 			if err == io.EOF {
-				response.JSONResponse(w, http.StatusBadRequest, "Нет файла")
+				api.JSONResponse(w, http.StatusBadRequest, "Нет файла")
 				return
 			}
 			if err != nil {
-				response.JSONResponse(w, http.StatusBadRequest, fmt.Sprintf("Ошибка чтения части: %v", err))
+				api.JSONResponse(w, http.StatusBadRequest, fmt.Sprintf("Ошибка чтения части: %v", err))
 				return
 			}
 
@@ -47,7 +56,7 @@ func UploadHandler(minioSvc *service.MinIOService, cfg *config.Config) http.Hand
 		ctx := context.Background()
 
 		minioSvc.Client.PutObject(ctx, cfg.Bucket, part.FileName(), part, -1, minio.PutObjectOptions{PartSize: defaultPartSize})
-		response.JSONResponse(w, http.StatusOK, "Файл успешно загружен")
+		api.JSONResponse(w, http.StatusOK, "Файл успешно загружен")
 
 	}
 
