192ba9e76865e6a7df02410c53a4cc5fc4c945e6
Eglantine <antonscheglow40o@gmail.com>
2025-04-17 09:25:57 +0300

    add graceful shutdown

--- services/file_upload_service/cmd/server/main.go
+++ services/file_upload_service/cmd/server/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Eglant1ne/simple_videohosting/services/file_upload_service/internal/config"
 	"github.com/Eglant1ne/simple_videohosting/services/file_upload_service/internal/handler"
@@ -20,10 +25,27 @@ func main() {
 	r := chi.NewRouter()
 	r.Get("/health", handler.HealthCheckHandler)
 	r.Post("/upload/video", handler.UploadHandler(minioSvc, &cfg, rabbitSvc))
-
-	log.Println("Server listening on :8080")
-
-	if err := http.ListenAndServe(":8080", r); err != nil {
-		log.Fatalf("server error: %v", err)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
 	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		log.Println("Server listening on :8080")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("server error: %v", err)
+		}
+	}()
+
+	<-done
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server Shutdown Failed: %+v", err)
+	}
+	log.Println("Server exited properly")
 }
